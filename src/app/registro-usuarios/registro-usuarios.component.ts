94d8faad2c0cbb997a77a4c40b4b700a53599e5a
Carlos Castillo Cueva <ing.elec.castillo@gmail.com>
2021-11-30 21:39:47 -0500

    agregando validaciones al formulario

--- src/app/registro-usuarios/registro-usuarios.component.ts
+++ src/app/registro-usuarios/registro-usuarios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../user.model';
 
 @Component({
@@ -12,26 +12,116 @@ export class RegistroUsuariosComponent implements OnInit {
   show:boolean = true;
 
   users:User[]=[
-    new User('Rodrigo','Perez','Rodri23','rodrigo.perez@correo.com',999324992,true),
-    new User('Martha','Gomez','Martha333','martha_gomez@correo.com',934748739,true)
+    new User('Rodrigo Fernando','Perez Gutierrez','Rodri23','rodrigo.perez@correo.com',999324992,true),
+    new User('Martha Alexandra','Gomez Silva','Martha333','martha_gomez@correo.com',934748739,true)
   ];
 
+  patternLetters = /^[a-zA-Z ñ]+$/;
+  patternUser = /^[0-9a-zA-Zñ]+$/;
+  patternEmail = /^[0-9a-zA-Z._-]+@[a-zA-Z]+?\.[a-zA-Z]{2,3}$/;
+
   formularioRegistro = new FormGroup({
-    name: new FormControl(''),
-    lastname: new FormControl(''),
-    username: new FormControl(''),
-    email: new FormControl(''),
-    phonenumber: new FormControl(''),
-    checkbox: new FormControl('')
+    name: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(3), 
+        Validators.maxLength(20), 
+        Validators.pattern(this.patternLetters)
+      ]),
+    lastname: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20), 
+        Validators.pattern(this.patternLetters)
+      ]),
+    username: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12), 
+        Validators.pattern(this.patternUser)
+      ]),
+    email: new FormControl('',
+      [
+        Validators.required,
+        Validators.pattern(this.patternEmail)
+      ]),
+    phonenumber: new FormControl('',
+      [
+        Validators.required,
+        Validators.min(111111111), 
+        Validators.max(999999999)
+      ]),
+    checkbox: new FormControl('',[Validators.requiredTrue])
   })
+
+  isInvalidField (field:string) {
+    return (
+      this.formularioRegistro.get(field)?.invalid && 
+      (this.formularioRegistro.get(field)?.dirty || this.formularioRegistro.get(field)?.touched)
+    )
+  }
 
   onResetForm(){
     this.formularioRegistro.reset();
   }
 
   onSaveForm(){
-    this.users.push(this.formularioRegistro.value);
-    this.onResetForm();  
+    if(this.formularioRegistro.valid){
+      this.users.push(this.formularioRegistro.value);
+      this.onResetForm();  
+    }
+  }
+
+  getErrorMessage (field:string) {
+    let message;
+
+    if (this.formularioRegistro.get(field)?.errors?.required) {
+      switch(field) {
+        case 'name':
+          message = 'Por favor, ingrese su nombre.';
+          break;
+        case 'lastname':
+          message = 'Por favor, ingrese su apellido.';
+          break;
+        case 'username':
+          message = 'Por favor, ingrese un nombre de usuario.';
+          break;
+        case 'email':
+          message = 'Por favor, ingrese su dirección de correo electrónico.';
+          break;
+        case 'phonenumber':
+          message = 'Por favor, ingrese su número de teléfono o celular.';
+          break;
+        default:
+          message = 'Debe aceptar los términos y condiciones.';
+      }
+    } else if (this.formularioRegistro.get(field)?.hasError('pattern')) {
+      switch(field) {
+        case 'username':
+          message = 'Formato incorrecto, ingrese solo letras y números.';
+          break;
+        case 'email':
+          message = 'Por favor, ingrese una dirección de correo electrónico válida (p.e. someone@example.com).';
+          break;
+        case 'phonenumber':
+          message = 'Por favor, ingrese su número de teléfono o celular.';
+          break;
+        default:
+          message = 'Formato incorrecto, ingrese solo letras.';
+      }
+    } else if (this.formularioRegistro.get(field)?.hasError('minlength')) {
+      const minLength = this.formularioRegistro.get(field)?.errors?.minlength.requiredLength;
+      message = `Ingrese mínimo ${minLength} caracteres`
+    } else if (this.formularioRegistro.get(field)?.hasError('maxlength')) {
+      const maxLength = this.formularioRegistro.get(field)?.errors?.maxlength.requiredLength;
+      message = `Ingrese máximo ${maxLength} caracteres`
+    } else if (this.formularioRegistro.get(field)?.hasError('min') || this.formularioRegistro.get(field)?.hasError('max')) {
+      message = `Ingrese un número de 9 dígitos.`
+    }
+
+    return message;
   }
 
   constructor() { }
